9b81229a205338caabbfe637fb01abb33042b5b1
lorens <lorens32645@gmail.com>
2023-02-10 18:55:39 +0300

    Day 3 - finish

--- service/services/views.py
+++ service/services/views.py
@@ -11,8 +11,19 @@ class SubscriptionView(ReadOnlyModelViewSet):
     # Достаём все поля.
     # queryset = Subscription.objects.all().prefetch_related('client').prefetch_related('client__user')
 
-    # Оптимизируем. Только нужные поля получаем.
+    # Оптимизируем. Только нужные поля получаем. Без учёта PlanSerializer (до его создания). Избегаем n+1.
+    # Запросы не будут перемножаться при увеличении количества данных
+
+    # Резюме:
+    # Prefetch() - избегаем n+1.
+    # only() - получаем только нужные поля.
+
+    # queryset = Subscription.objects.all().prefetch_related(
+    #     Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')))
+
+    # Оптимизируем. Только нужные поля получаем. C учётом PlanSerializer
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')))
 
     serializer_class = SubscriptionSerializer
